4bb9e39053d67b29a73ac65265a95d883502b6ef
Faezeh <FVakili@users.noreply.github.com>
2018-04-18 17:37:52 +1000

    nav commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {Routes , RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AccountsComponent } from './accounts/accounts.component';
 import { UsersComponent } from './users/users.component';
+import { EditUserComponent } from './users/edit-user/edit-user.component';
 
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'users', component: UsersComponent},
+  { path: 'accounts', component: AccountsComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AccountsComponent,
-    UsersComponent
+    UsersComponent,
+    EditUserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
