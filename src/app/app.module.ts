adf6ab466439c2de0abb13bff3140f99e5739434
Faezeh <FVakili@users.noreply.github.com>
2018-04-18 16:16:15 +1000

    Bootstrap Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AccountsComponent } from './accounts/accounts.component';
+import { UsersComponent } from './users/users.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AccountsComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule
